3699dfbf68b7ac8e689e49c0d6533cf86c48e645
chdsapphire <123945062+chdsapphire@users.noreply.github.com>
2023-01-30 13:38:36 +0200

    Add files via upload

---
+++ User ID Helper Погані.user.js
@@ -0,0 +1,95 @@
+// ==UserScript==
+// @name User ID Helper Погані
+// @namespace http://example.com/
+// @version 1.0
+// @description Adds a helper message near user ID
+// @author Me
+// @match http://www.charmdate.com/**
+// @grant none
+// ==/UserScript==
+
+(function() {
+'use strict';
+
+let links = document.querySelectorAll("td a");
+
+links.forEach(function(link) {
+if (link.textContent.includes("CM31876079")) {
+let parent = link.parentElement;
+let newElement = document.createElement("div");
+newElement.innerHTML = "Не слати спам через невидимий символ";
+newElement.style.backgroundColor = "yellow";
+newElement.style.color = "black";
+parent.appendChild(newElement);
+}
+});
+})();
+
+
+(function() {
+'use strict';
+
+let links = document.querySelectorAll("td a");
+
+links.forEach(function(link) {
+if (link.textContent.includes("CM27508817")) {
+let parent = link.parentElement;
+let newElement = document.createElement("div");
+newElement.innerHTML = "Don't like scam with gifts";
+newElement.style.backgroundColor = "yellow";
+newElement.style.color = "black";
+parent.appendChild(newElement);
+}
+});
+})();
+
+(function() {
+'use strict';
+
+let links = document.querySelectorAll("td a");
+
+links.forEach(function(link) {
+if (link.textContent.includes("CM34498939")) {
+let parent = link.parentElement;
+let newElement = document.createElement("div");
+newElement.innerHTML = "Високе очикування в швидкості відповідей";
+newElement.style.backgroundColor = "yellow";
+newElement.style.color = "black";
+parent.appendChild(newElement);
+}
+});
+})();
+
+(function() {
+'use strict';
+
+let links = document.querySelectorAll("td a");
+
+links.forEach(function(link) {
+if (link.textContent.includes("CM313497")) {
+let parent = link.parentElement;
+let newElement = document.createElement("div");
+newElement.innerHTML = "Фут фетіш, якщо фото без ніг жалоба";
+newElement.style.backgroundColor = "yellow";
+newElement.style.color = "black";
+parent.appendChild(newElement);
+}
+});
+})();
+
+(function() {
+'use strict';
+
+let links = document.querySelectorAll("td a");
+
+links.forEach(function(link) {
+if (link.textContent.includes("CM97535658")) {
+let parent = link.parentElement;
+let newElement = document.createElement("div");
+newElement.innerHTML = "ЛОХ";
+newElement.style.backgroundColor = "yellow";
+newElement.style.color = "black";
+parent.appendChild(newElement);
+}
+});
+})();
