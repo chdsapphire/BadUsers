5df2d9bf25f176cb6abee58bdadea6d9448d7022
chdsapphire <123945062+chdsapphire@users.noreply.github.com>
2023-01-30 15:24:29 +0200

    Update User ID Helper Погані.user.js

--- User ID Helper Погані.user.js
+++ User ID Helper Погані.user.js
@@ -1,11 +1,20 @@
 // ==UserScript==
-// @name User ID Helper Погані
+// @name User ID Helper BadUsers
 // @namespace http://example.com/
-// @version 1.0
+// @version 1.01
 // @description Adds a helper message near user ID
 // @author Me
 // @match http://www.charmdate.com/**
-// @grant none
+// @match http://www1.charmdate.com/**
+// @match http://www2.charmdate.com/**
+// @match http://www3.charmdate.com/**
+// @match http://www4.charmdate.com/**
+// @match http://www5.charmdate.com/**
+// @match http://www6.charmdate.com/**
+// @match http://www7.charmdate.com/**
+// @updateURL   https://github.com/chdsapphire/BadUsersCharmDate/blob/main/User%20ID%20Helper%20%D0%9F%D0%BE%D0%B3%D0%B0%D0%BD%D1%96.user.js
+// @downloadURL https://github.com/chdsapphire/BadUsersCharmDate/blob/main/User%20ID%20Helper%20%D0%9F%D0%BE%D0%B3%D0%B0%D0%BD%D1%96.user.js
+// ==/UserScript==
 // ==/UserScript==
 
 (function() {
@@ -86,7 +95,42 @@ links.forEach(function(link) {
 if (link.textContent.includes("CM97535658")) {
 let parent = link.parentElement;
 let newElement = document.createElement("div");
-newElement.innerHTML = "ЛОХ";
+newElement.innerHTML = "ЛОХ2211";
+newElement.style.backgroundColor = "yellow";
+newElement.style.color = "black";
+parent.appendChild(newElement);
+}
+});
+})();
+
+
+(function() {
+'use strict';
+
+let links = document.querySelectorAll("td a");
+
+links.forEach(function(link) {
+if (link.textContent.includes("CM97535658")) {
+let parent = link.parentElement;
+let newElement = document.createElement("div");
+newElement.innerHTML = "2";
+newElement.style.backgroundColor = "yellow";
+newElement.style.color = "black";
+parent.appendChild(newElement);
+}
+});
+})();
+
+(function() {
+'use strict';
+
+let links = document.querySelectorAll("td a");
+
+links.forEach(function(link) {
+if (link.textContent.includes("CM97535658")) {
+let parent = link.parentElement;
+let newElement = document.createElement("div");
+newElement.innerHTML = "4";
 newElement.style.backgroundColor = "yellow";
 newElement.style.color = "black";
 parent.appendChild(newElement);
