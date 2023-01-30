dcf40068d9d4213d9c2c5c0af90af738d85c02a4
chdsapphire <123945062+chdsapphire@users.noreply.github.com>
2023-01-30 16:13:08 +0200

    Update User ID Helper Погані.user.js

--- User ID Helper Погані.user.js
+++ User ID Helper Погані.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name User ID Helper BadUsers
 // @namespace http://example.com/
-// @version 1.03
+// @version 1.04
 // @description Adds a helper message near user ID
 // @author Me
 // @match http://www.charmdate.com/**
